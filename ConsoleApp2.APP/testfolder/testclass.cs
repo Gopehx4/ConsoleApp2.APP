4c6e475f0fe9dd3998583020e0b5a621e0620aa8
Gopehx4 <51216834+Gopehx4@users.noreply.github.com>
2023-09-16 21:51:48 -0600

    testing folderr

---
+++ ConsoleApp2.APP/testfolder/testclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2.APP.testfolder
+{
+    internal class testclass
+    {
+    }
+}
+
+
+testig class
